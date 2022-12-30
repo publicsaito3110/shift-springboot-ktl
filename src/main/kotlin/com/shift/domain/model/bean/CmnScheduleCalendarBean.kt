78afda55a6877b6565a20972418533201c0ba5d1
publicsaito3110 <public.business.saito3110@gmail.com>
2022-12-30 23:00:05 +0900

    add: CmnScheduleCalendarBeanの追加

---
+++ src/main/kotlin/com/shift/domain/model/bean/CmnScheduleCalendarBean.kt
@@ -0,0 +1,27 @@
+package com.shift.domain.model.bean
+
+import lombok.AllArgsConstructor
+import lombok.NoArgsConstructor
+
+/**
+ * @author saito
+ *
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+class `CmnScheduleCalendarBean` {
+
+    var year: Int = 0
+
+    var month: Int = 0
+
+    var lastDateYmd: String? = null
+
+    var calendarList: List<Int>? = null
+
+    var nowYm: String? = null
+
+    var nextYm: String? = null
+
+    var beforeYm: String? = null
+}
