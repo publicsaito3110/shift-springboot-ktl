b4378270ca67ad9184a2b782223f7a0aaf1b9848
publicsaito3110 <public.business.saito3110@gmail.com>
2023-01-03 15:34:17 +0900

    add: Constの追加

---
+++ src/main/kotlin/com/shift/common/Const.kt
@@ -0,0 +1,21 @@
+package com.shift.common
+
+/**
+ * @author saito
+ *
+ */
+public class Const {
+    private constructor() {
+    // インスタンス化を禁止
+    }
+
+    companion object {
+
+        // Schedule
+        const val SCHEDULE_RECORDABLE_MAX_DIVISION: Int = 7
+
+        // DM
+        const val DM_READ_FLG: String = "1"
+    }
+
+}
