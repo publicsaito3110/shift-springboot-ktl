0f9a92ef277a39521c7ddea3d8c6e19136480360
publicsaito3110 <public.business.saito3110@gmail.com>
2023-01-04 12:08:09 +0900

    add: UserRepositoryの追加

---
+++ src/main/kotlin/com/shift/domain/repository/UserRepository.kt
@@ -0,0 +1,13 @@
+package com.shift.domain.repository
+
+import com.shift.domain.model.entity.UserEntity
+
+
+/**
+ * @author saito
+ *
+ */
+//@Repository
+interface UserRepository: BaseRepository<UserEntity, String> {
+
+}
