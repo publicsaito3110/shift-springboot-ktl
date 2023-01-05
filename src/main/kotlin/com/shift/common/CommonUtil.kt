772169fe42dcb6d88a1ddb09a5b6bb90b44ea0ad
publicsaito3110 <public.business.saito3110@gmail.com>
2023-01-05 21:47:13 +0900

    fix: バリデーション判定処理の追加

--- src/main/kotlin/com/shift/common/CommonUtil.kt
+++ src/main/kotlin/com/shift/common/CommonUtil.kt
@@ -23,12 +23,42 @@ class CommonUtil {
          */
         fun changeEmptyByNull(value: String?): String {
 
-            //nullのとき
+            // nullのとき
             if (value == null) {
                 return ""
             }
 
             return value
+        }
+
+
+        /**
+         * バリデーション判定処理
+         *
+         * 正規表現のパターンと一致しているか判定する<br>
+         * ただし、valueがnullまたは正規表現が適切でないときは必ず失敗する
+         *
+         * @param value 判定させる値
+         * @param regex 正規表現のパターン
+         * @return Boolean 正規表現のパターンマッチの結果<br>
+         * true: 正規表現のパターンと一致しているとき, false: 正規表現のパターンと一致していないとき
+         */
+        fun isSuccessValidation(value: String?, regex: String): Boolean {
+
+            try {
+
+                // 正規表現のパターンとマッチしているとき
+                val regexRg: Regex = Regex(regex)
+                if (value != null && value.matches(regexRg)) {
+                    return true
+                }
+
+                return false
+            } catch (e: Exception) {
+
+                // 例外発生時、falseを返す
+                return false
+            }
         }
     }
 }
