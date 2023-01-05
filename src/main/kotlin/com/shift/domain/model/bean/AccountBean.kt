985d9244e5ac51cc361d370225d8b9b21f1f9746
publicsaito3110 <public.business.saito3110@gmail.com>
2023-01-06 08:45:06 +0900

    add: AccountBeanの追加

---
+++ src/main/kotlin/com/shift/domain/model/bean/AccountBean.kt
@@ -0,0 +1,42 @@
+package com.shift.domain.model.bean
+
+import com.shift.domain.model.entity.UserEntity
+import lombok.AllArgsConstructor
+import lombok.NoArgsConstructor
+
+/**
+ * @author saito
+ *
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+class AccountBean {
+
+    var userId: String? = null
+
+    var name: String? = null
+
+    var nameKana: String? = null
+
+    var isAdmin: Boolean = false
+
+
+    /**
+     * [Constractor] UserEntity
+     *
+     * セッションとして保持するためのユーザ情報をUserEntityから取得し、セットする<br>
+     * ただし、nullのときは何もしない
+     *
+     * @param userEntity セッションとして保持したいユーザ
+     * @return void
+     */
+    constructor(userEntity: UserEntity?) {
+
+        if (userEntity != null) {
+            userId = userEntity.id
+            name = userEntity.name
+            nameKana = userEntity.nameKana
+            isAdmin = userEntity.adminFlgFormatTF()
+        }
+    }
+}
