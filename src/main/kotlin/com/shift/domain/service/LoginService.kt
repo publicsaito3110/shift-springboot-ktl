cfc3ff8670caa3e77884e1bbc377bd54b6159a81
publicsaito3110 <public.business.saito3110@gmail.com>
2023-01-05 21:45:06 +0900

    fix: ログイン認証設定処理の追加

--- src/main/kotlin/com/shift/domain/service/LoginService.kt
+++ src/main/kotlin/com/shift/domain/service/LoginService.kt
@@ -48,4 +48,96 @@ class LoginService: BaseService() {
 //            val dmUnreadCountDto: DmUnreadCountDto = selectUnreadMsgCountByLoginUser(loginUser) TODO DM未読処理
             generateSession(userEntity)
         }
+
+        // Beanにセット
+        val loginAuthBean: LoginAuthBean = LoginAuthBean()
+        loginAuthBean.isLogin = isLogin
+        loginAuthBean.errorMassage = errorMassage
+        return loginAuthBean
+    }
+
+
+    /**
+     * セッション処理
+     *
+     * ログイン情報からログイン済みを識別するセッションをセットする<br>
+     * ただし、isLoginがfalse(ログインが認証されていない)ときはセッションをセットしない
+     *
+     * @param userEntity Repositoryから取得したユーザ
+     * @return Boolean セッションのセットの判定<br>
+     * true: セッションのセットに成功したとき<br>
+     * false: セッションのセットに失敗したとき
+     */
+    private fun generateSession(userEntity: UserEntity?): Boolean {
+
+        // ユーザが存在しないとき
+        if (userEntity == null) {
+            return false
+        }
+
+        // セッションを完全に削除
+        httpSession.invalidate()
+
+        // アカウント情報をセッションをセット
+        val accountBean: AccountBean = AccountBean(userEntity)
+        httpSession.setAttribute(Const.SESSION_KEYWORD_ACCOUNT_BEAN, accountBean)
+        return true
+    }
+
+
+    /**
+     * ログイン可能ユーザ判定処理
+     *
+     * ログイン情報(userEntity)を基に取得したユーザーがログイン可能であるか判定する<br>
+     * 一致するユーザーがいないまたは退職済みのときはログインが不可になる<br>
+     * また、ログインが不可であるときはエラーメッセージをフィールド(errorMassage)にセットする
+     *
+     * @param userEntity Repositoryから取得したユーザ
+     * @return boolean<br>
+     * true: ログイン情報から一致するユーザーかつ退職済みでないユーザであるとき<br>
+     * false: ログイン情報から一致するユーザがいないまたは退職済みであるとき
+     */
+    private fun isCheckLoginUser(userEntity: UserEntity?): Boolean {
+
+        //　ログイン情報からユーザーを取得できなかったとき
+        if (userEntity == null) {
+
+            //　エラーメッセージをセットし、falseを返す
+            errorMassage = "IDまたはパスワードが違います"
+            return false
+        }
+
+        //　退職済みだったとき
+        if (userEntity.delFlgFormatTF()) {
+
+            //　エラーメッセージをセットし、falseを返す
+            errorMassage = "このユーザーは現在ログインできません"
+            return false
+        }
+        return true
+    }
+
+
+    /**
+     * [Repository] ユーザ検索処理
+     *
+     * userIdと一致するユーザIDのユーザを取得する<br>
+     * ただし、一致するユーザーがいないときはnullとなる
+     *
+     * @param userId 取得したいユーザのユーザID
+     * @return UserEntity? ユーザIDと一致したユーザ
+     */
+    private fun selectUser(userId: String?): UserEntity? {
+
+        // loginUserと一致するユーザを検索
+        val trimUserId: String = CommonUtil.changeEmptyByNull(userId)
+        val userEntityOpt: Optional<UserEntity> = userRepository.findById(trimUserId)
+
+        // ユーザが存在しないとき
+        if (!userEntityOpt.isPresent) {
+            return null
+        }
+
+        return userEntityOpt.get()
+    }
 }
