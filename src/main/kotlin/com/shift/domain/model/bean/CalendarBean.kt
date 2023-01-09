f3ae2490adfbe10023f3d76f5d80dcbb0f9393a4
publicsaito3110 <public.business.saito3110@gmail.com>
2023-01-09 15:38:43 +0900

    fix: CalendarServiceの変更に伴う修正

--- src/main/kotlin/com/shift/domain/model/bean/CalendarBean.kt
+++ src/main/kotlin/com/shift/domain/model/bean/CalendarBean.kt
@@ -1,5 +1,7 @@
 package com.shift.domain.model.bean
 
+import com.shift.domain.model.entity.ScheduleEntity
+import com.shift.domain.model.entity.ScheduleTimeEntity
 import lombok.AllArgsConstructor
 import lombok.NoArgsConstructor
 
@@ -17,9 +19,15 @@ class CalendarBean {
 
     var nowYm: String? = null
 
-    var calendarList: List<Int>? = null
+    var calendarList: List<Int?>? = null
 
     var afterYm: String? = null
 
     var beforeYm: String? = null
+
+    var scheduleEntity: ScheduleEntity? = null
+
+    var scheduleTimeEntity: ScheduleTimeEntity? = null
+
+    var isScheduleAllArray: Array<Array<Boolean?>>? = null
 }
