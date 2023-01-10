53630cb4aa4e6a7f77830b4fe24d5f937a12c861
publicsaito3110 <public.business.saito3110@gmail.com>
2023-01-10 10:16:04 +0900

    add: CmnScheduleLogicの追加

---
+++ src/main/kotlin/com/shift/common/CmnScheduleLogic.kt
@@ -0,0 +1,13 @@
+package com.shift.common
+
+import com.shift.domain.model.bean.collection.ScheduleTimeBean
+import com.shift.domain.model.entity.ScheduleTimeEntity
+
+
+/**
+ * @author saito
+ *
+ */
+class CmnScheduleLogic {
+
+}
