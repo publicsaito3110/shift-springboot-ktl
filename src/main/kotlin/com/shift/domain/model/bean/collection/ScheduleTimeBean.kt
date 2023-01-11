216dfeb0d1b85e8483b130382ca10123be71196b
publicsaito3110 <public.business.saito3110@gmail.com>
2023-01-11 15:12:13 +0900

    fix: 時間フォーマット変換処理の追加

--- src/main/kotlin/com/shift/domain/model/bean/collection/ScheduleTimeBean.kt
+++ src/main/kotlin/com/shift/domain/model/bean/collection/ScheduleTimeBean.kt
@@ -37,4 +37,126 @@ class ScheduleTimeBean {
         this.endHm = endHm
         this.restHm = restHm
     }
+
+
+    /**
+     * startHm時間フォーマット変換処理<br></br>
+     * 時間フォーマット(HH:MM)に変換
+     *
+     * @param void
+     * @return String 時間フォーマット(HH:MM)
+     */
+    fun startHmsFormatTime(): String? {
+        return changeHmFormatTime(startHm)
+    }
+
+
+    /**
+     * startHm時間取得処理<br></br>
+     * startHmの時間(hour)を取得する
+     *
+     * @param void
+     * @return String startHmの時間(hour)
+     */
+    fun startHmForHour(): String? {
+        return startHm!!.substring(0, 2)
+    }
+
+
+    /**
+     * startHm分取得処理<br></br>
+     * startHmの分(minutes)を取得する
+     *
+     * @param void
+     * @return String startHmの分(minutes)
+     */
+    fun startHmForMinutes(): String? {
+        return startHm!!.substring(2, 4)
+    }
+
+
+    /**
+     * endHm時間フォーマット変換処理<br></br>
+     * 時間フォーマット(HH:MM)に変換
+     *
+     * @param void
+     * @return String 時間フォーマット(HH:MM)
+     */
+    fun endHmsFormatTime(): String? {
+        return changeHmFormatTime(endHm)
+    }
+
+
+    /**
+     * endHm時間取得処理<br></br>
+     * endHmの時間(hour)を取得する
+     *
+     * @param void
+     * @return String endHmの時間(hour)
+     */
+    fun endHmForHour(): String? {
+        return endHm!!.substring(0, 2)
+    }
+
+
+    /**
+     * endHm分取得処理<br></br>
+     * endHmの分(minutes)を取得する
+     *
+     * @param void
+     * @return String endHmの分(minutes)
+     */
+    fun endHmForMinutes(): String? {
+        return endHm!!.substring(2, 4)
+    }
+
+
+    /**
+     * restHm時間フォーマット変換処理<br></br>
+     * 時間フォーマット(HH:MM)に変換
+     *
+     * @param void
+     * @return String 時間フォーマット(HH:MM)
+     */
+    fun restHmsFormatTime(): String? {
+        return changeHmFormatTime(restHm)
+    }
+
+
+    /**
+     * restHm時間取得処理<br></br>
+     * restHmの時間(hour)を取得する
+     *
+     * @param void
+     * @return String restHmの時間(hour)
+     */
+    fun restHmForHour(): String? {
+        return restHm!!.substring(0, 2)
+    }
+
+
+    /**
+     * restHm分取得処理<br></br>
+     * restHmの分(minutes)を取得する
+     *
+     * @param void
+     * @return String restHmの分(minutes)
+     */
+    fun restHmForMinutes(): String? {
+        return restHm!!.substring(2, 4)
+    }
+
+
+    /**
+     * [private 共通処理] 時間フォーマット変換処理<br></br>
+     * 時間フォーマット(HH:MM)に変換
+     *
+     * @param hm 変換したい値
+     * @return String 時間フォーマット(HH:MM)
+     */
+    private fun changeHmFormatTime(hm: String?): String? {
+
+        //フォーマットをhh:mmに変換し、返す
+        return hm!!.substring(0, 2) + ":" + hm!!.substring(2, 4)
+    }
 }
