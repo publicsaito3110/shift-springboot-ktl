40359ef41a6093f15ce364237aec52e626b3e2bb
publicsaito3110 <public.business.saito3110@gmail.com>
2023-01-11 15:17:17 +0900

    fix: カレンダー表示機能の追加に伴う修正

--- src/main/kotlin/com/shift/controller/CalendarController.kt
+++ src/main/kotlin/com/shift/controller/CalendarController.kt
@@ -1,11 +1,14 @@
 package com.shift.controller
 
+import com.shift.common.Const
 import com.shift.domain.model.bean.CalendarBean
 import com.shift.domain.service.CalendarService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 /**
  * @author saito
@@ -25,11 +28,17 @@ class CalendarController : BaseController() {
      * @return String
      */
     @RequestMapping("/calendar")
-    fun calendar(model: Model): String {
+    fun calendar(@RequestParam(value="ym",required=false) ym: String?, authentication: Authentication, model: Model): String {
+
+        // ログインしているユーザのIDを取得
+        val loginUser: String = authentication.name
 
         // Service
-        val calendarBean: CalendarBean = calendarService.calendar()
+        val calendarBean: CalendarBean = calendarService.calendar(ym, loginUser)
         model.addAttribute("bean", calendarBean)
-        return "calendar"
+        model.addAttribute("scheduleHtmlColorArray", Const.SCHEDULE_HTML_CLASS_DISPLAY_COLOR_ARRAY)
+        model.addAttribute("scheduleHtmlBgColorArray", Const.SCHEDULE_HTML_CLASS_DISPLAY_BG_COLOR_ARRAY)
+        // View
+        return "view/calendar/calendar"
     }
 }
