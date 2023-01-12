31958840d8103b23b18d8f6efa0b1f05127e6a7d
publicsaito3110 <public.business.saito3110@gmail.com>
2023-01-13 05:54:40 +0900

    add: LogoutServiceの追加

---
+++ src/main/kotlin/com/shift/domain/service/LogoutService.kt
@@ -0,0 +1,51 @@
+package com.shift.domain.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import javax.servlet.http.HttpSession
+
+
+/**
+ * @author saito
+ *
+ */
+@Service
+@Transactional
+class LogoutService: BaseService() {
+
+    @Autowired
+    private lateinit var httpSession: HttpSession
+
+
+    /**
+     * [Service] ログアウト機能 (/logout)
+     *
+     * @param
+     * @return
+     */
+    fun logout(): Boolean {
+
+        // セッションを完全に削除
+        removeSession()
+        return true
+    }
+
+
+    /**
+     * セッション削除処理
+     *
+     * 保持しているセッションを完全に削除する
+     *
+     * @param
+     * @return Boolean セッションの削除の判定<br>
+     * true: セッションの削除に成功したとき<br>
+     * false: セッションの削除に失敗したとき
+     */
+    private fun removeSession(): Boolean {
+
+        // セッションを完全に削除
+        httpSession.invalidate()
+        return true
+    }
+}
