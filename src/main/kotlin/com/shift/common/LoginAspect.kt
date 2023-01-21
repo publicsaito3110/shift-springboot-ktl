7a97e7501078eb06b58e829d3bdf07715e440543
publicsaito3110 <public.business.saito3110@gmail.com>
2023-01-21 18:42:42 +0900

    fix: ログイン判定処理の変更

--- src/main/kotlin/com/shift/common/LoginAspect.kt
+++ src/main/kotlin/com/shift/common/LoginAspect.kt
@@ -25,17 +25,15 @@ class LoginAspect {
 
 
     /**
-     * セッション判定処理(AOP)
+     * セッション判定処理 (AOP)
      *
-     *
-     * Controller実行前にセッション及びURI制限を行う<br></br>
+     * Controller実行前にセッション及びURI制限を行う<br>
      * セッションが未保持のとき、<br></br>
-     * ・セッションの未保持が許容されていないURI: ログイン画面へ強制的に遷移<br></br>
-     * ・セッションの未保持が許容されているURI: そのまま実行<br></br>
-     * セッションが保持されているとき、<br></br>
-     * ・ログイン関連のURI: 強制的にホーム画面へ遷移<br></br>
+     * ・セッションの未保持が許容されていないURI: ログイン画面へ強制的に遷移<br>
+     * ・セッションの未保持が許容されているURI: そのまま実行<br>
+     * セッションが保持されているとき、<br>
+     * ・ログイン関連のURI: 強制的にホーム画面へ遷移<br>
      * ・ログイン関連以外のURI: そのまま実行
-     *
      *
      * @param joinPoint ProceedingJoinPoint
      * @return Object
@@ -46,7 +44,7 @@ class LoginAspect {
 
         val targetControllerObj: Any = joinPoint.target
 
-        // Sessionを取得
+        // ログインセッションを取得
         val accountBean: AccountBean? = httpSession.getAttribute(Const.SESSION_KEYWORD_ACCOUNT_BEAN) as AccountBean
 
         // 現在のURIを取得
@@ -54,13 +52,10 @@ class LoginAspect {
             (RequestContextHolder.getRequestAttributes() as ServletRequestAttributes).request
         val nowUri: String = requset.requestURI
 
-        val isA: Boolean = requestMappingHandler.useTrailingSlashMatch()
-//        requestMappingHandler.
-
-        //Sessionが存在しないとき
+        // ログインセッションが存在しないとき
         if (accountBean == null) {
 
-            // Sessionの未保持を許容するURI
+            // ログインセッションの未保持を許容するURI
             val sessionIgnoreUriArray: Array<String> = arrayOf(
                 "/",
                 "/login",
