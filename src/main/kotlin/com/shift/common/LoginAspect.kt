cdec4ee904b3a26423b307b3616560400f581351
publicsaito3110 <public.business.saito3110@gmail.com>
2023-01-23 20:31:35 +0900

    fix: 未ログイン時のエラーハンドリング処理の追加

--- src/main/kotlin/com/shift/common/LoginAspect.kt
+++ src/main/kotlin/com/shift/common/LoginAspect.kt
@@ -5,10 +5,10 @@ import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController
 import org.springframework.stereotype.Component
 import org.springframework.web.context.request.RequestContextHolder
 import org.springframework.web.context.request.ServletRequestAttributes
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpSession
 
@@ -20,15 +20,12 @@ class LoginAspect {
     @Autowired
     private lateinit var httpSession: HttpSession
 
-    @Autowired
-    private lateinit var requestMappingHandler: RequestMappingHandlerMapping
-
 
     /**
      * セッション判定処理 (AOP)
      *
      * Controller実行前にセッション及びURI制限を行う<br>
-     * セッションが未保持のとき、<br></br>
+     * セッションが未保持のとき、<br>
      * ・セッションの未保持が許容されていないURI: ログイン画面へ強制的に遷移<br>
      * ・セッションの未保持が許容されているURI: そのまま実行<br>
      * セッションが保持されているとき、<br>
@@ -36,76 +33,51 @@ class LoginAspect {
      * ・ログイン関連以外のURI: そのまま実行
      *
      * @param joinPoint ProceedingJoinPoint
-     * @return Object
+     * @return Object Controller実行時の戻り値
      */
     @Around("execution(* *..*Controller.*(..))")
     @Throws(Throwable::class)
     fun executeSession(joinPoint: ProceedingJoinPoint): Any? {
 
-        val targetControllerObj: Any = joinPoint.target
+        // リクエストを受け取ったControllerを取得
+        val controllerObj: Any = joinPoint.target
 
-        // ログインセッションを取得
+        // エラーハンドリングするControllerのとき、Controllerの処理を実行
+        if (controllerObj is BasicErrorController) {
+            return joinPoint.proceed()
+        }
+
+        // セッションを取得
         val accountBean: AccountBean? = httpSession.getAttribute(Const.SESSION_KEYWORD_ACCOUNT_BEAN) as AccountBean
 
         // 現在のURIを取得
-        val requset: HttpServletRequest =
-            (RequestContextHolder.getRequestAttributes() as ServletRequestAttributes).request
-        val nowUri: String = requset.requestURI
+        val request: HttpServletRequest = (RequestContextHolder.getRequestAttributes() as ServletRequestAttributes?)!!.request
+        val nowUri: String = request.requestURI
 
-        // ログインセッションが存在しないとき
+        // セッションが存在しないとき
         if (accountBean == null) {
 
-            // ログインセッションの未保持を許容するURI
-            val sessionIgnoreUriArray: Array<String> = arrayOf(
-                "/",
-                "/login",
-                "/login/auth",
-                "/login/forgot-password",
-                "/login/forgot-password/send",
-                "/login/forgot-password/reset",
-                "/login/forgot-password/reset/auth",
-                "/login/forgot-password/reset/modify",
-                "/login/forgot-id",
-                "/login/forgot-id/send",
-                "/login/error",
-                "/logout"
-            )
+            // セッションの未保持を許容するURI
+            val patternSessionIgnoreUri: String = "/|/logout|/login|^/login/.*$"
 
-            //sessionIgnoreUriArrayに含まれていないとき
-//            if (!Arrays.asList(sessionIgnoreUriArray).contains(nowUri)) {
-//
-//                //ログイン画面へ強制的に遷移
-//                val modelAndView = ModelAndView()
-//                modelAndView.viewName = "redirect:/login/error"
-//                return modelAndView
-//            }
-//            return joinPoint.proceed()
-//        }
-
-            //ログインに関するURI
-            val loginUriArray = arrayOf(
-                "/login",
-                "/login/auth",
-                "/login/forgot-password",
-                "/login/forgot-password/send",
-                "/login/forgot-password/reset",
-                "/login/forgot-password/reset/auth",
-                "/login/forgot-password/reset/modify",
-                "/login/forgot-id",
-                "/login/forgot-id/send",
-                "/login/error"
-            )
-
-            //ログインに関するURIにアクセスかつSessionが存在しているとき
-//        if (Arrays.asList(loginUriArray).contains(nowUri) && accountBean != null) {
-//
-//            //ホーム画面へ強制的に遷移
-//            val modelAndView = ModelAndView()
-//            modelAndView.viewName = "redirect:/home"
-//            return modelAndView
-//        }
-        }
-            //Controllerの処理を実行し、オブジェクトを返す
+            // 現在のURIがセッション未保持を許容するURIに含まれていないとき、ログイン画面へ強制的に遷移
+            if (!CommonUtil.isSuccessValidation(nowUri, patternSessionIgnoreUri)) {
+                return "redirect:/login/error"
+            }
             return joinPoint.proceed()
+        }
+
+        // ログインに関するURI
+        val patternLoginUri: String = "/login|^/login/.*\$"
+
+        // ログインに関するURIにアクセスかつSessionが存在しているとき
+        if (CommonUtil.isSuccessValidation(nowUri, patternLoginUri)) {
+
+            // ホーム画面へ強制的に遷移
+            return "redirect:/home"
+        }
+
+        // Controllerの処理を実行
+        return joinPoint.proceed()
     }
 }
