5567220972d59da8e9088465bba182206d8b471f
publicsaito3110 <public.business.saito3110@gmail.com>
2023-01-31 07:29:56 +0900

    fix: SQL: スケジュール時間区分検索処理の追加

--- src/main/kotlin/com/shift/domain/repository/ScheduleTimeRepository.kt
+++ src/main/kotlin/com/shift/domain/repository/ScheduleTimeRepository.kt
@@ -14,7 +14,7 @@ interface ScheduleTimeRepository: BaseRepository<ScheduleTimeEntity, Int> {
 
 
     /**
-     * [DB]スケジュール時間区分取得処理
+     * [Repository] スケジュール時間区分取得処理
      *
      * 取得したい日付(ymd)から該当するスケジュール時間区分を取得する<br></br>
      * また、現在日(ymd)に該当するスケジュール時間区分が複数登録されているときは最新のスケジュール時間区分が取得される<br></br>
@@ -23,6 +23,6 @@ interface ScheduleTimeRepository: BaseRepository<ScheduleTimeEntity, Int> {
      * @param ymd 取得したいスケジュール時間区分の日付(YYYYMMDD)
      * @return ScheduleTimeEntity
      */
-    @Query(value = " SELECT s.* FROM schedule_time s WHERE s.end_ymd = (SELECT MIN(c.end_ymd) FROM schedule_time c WHERE :ymd <= c.end_ymd) AND s.id = (SELECT MAX(h.id) FROM schedule_time h WHERE h.end_ymd = (SELECT MIN(e.end_ymd) FROM schedule_time e WHERE :ymd <= e.end_ymd))", nativeQuery = true)
+    @Query(value = "SELECT s.* FROM schedule_time s WHERE s.end_ymd = (SELECT MIN(c.end_ymd) FROM schedule_time c WHERE :ymd <= c.end_ymd) AND s.id = (SELECT MAX(h.id) FROM schedule_time h WHERE h.end_ymd = (SELECT MIN(e.end_ymd) FROM schedule_time e WHERE :ymd <= e.end_ymd))", nativeQuery = true)
     fun selectScheduleTimeByYmd(ymd: String?): ScheduleTimeEntity?
 }
