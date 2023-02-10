d077def984e7746d7e63cd0273f5e26d8dff7212
publicsaito3110 <public.business.saito3110@gmail.com>
2023-02-10 21:01:57 +0900

    fix: ModelAttributeの追加

--- src/main/kotlin/com/shift/controller/HomeController.kt
+++ src/main/kotlin/com/shift/controller/HomeController.kt
@@ -1,5 +1,7 @@
 package com.shift.controller
 
+import com.shift.common.Const
+import com.shift.domain.model.bean.HomeBean
 import com.shift.domain.service.HomeService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.Authentication
@@ -23,19 +25,21 @@ class HomeController : BaseController() {
      * [Controller] ホーム画面表示機能 (/home)
      *
      * @param ymd 1週間カレンダーに表示したい日付 (Not required)
-     * @param authentication
-     * @param model
+     * @param authentication Authentication
+     * @param model Model
      * @return String
      */
     @RequestMapping("/home")
-    fun home(@RequestParam(value="ymd",required=false) ymd: String, authentication: Authentication, model: Model): String {
+    fun home(@RequestParam(value="ymd",required=false) ymd: String?, authentication: Authentication, model: Model): String {
 
         // ログインユーザIDを取得
         val loginUser: String = authentication.name
 
         // Service
-        homeService.home(ymd, loginUser)
+        val bean: HomeBean = homeService.home(ymd, loginUser)
+        model.addAttribute("bean", bean)
+        model.addAttribute("scheduleHtmlBgColorArray", Const.SCHEDULE_HTML_CLASS_DISPLAY_BG_COLOR_ARRAY)
         // View
-        return "view/home"
+        return "view/home/home"
     }
 }
