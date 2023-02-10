e5523b35ed9963a1afc1f072e7356820404a81c4
publicsaito3110 <public.business.saito3110@gmail.com>
2023-02-11 08:38:15 +0900

    fix: HTML対応処理の追加

--- src/main/kotlin/com/shift/domain/model/bean/collection/NewsContentBean.kt
+++ src/main/kotlin/com/shift/domain/model/bean/collection/NewsContentBean.kt
@@ -1,5 +1,7 @@
 package com.shift.domain.model.bean.collection
 
+import com.shift.common.CommonLogic
+import com.shift.common.Const
 import com.shift.domain.model.entity.NewsEntity
 import lombok.AllArgsConstructor
 import lombok.NoArgsConstructor
@@ -40,5 +42,41 @@ class NewsContentBean {
         this.category = newsEntity.category
         this.title = newsEntity.title
         this.content = newsEntity.content
+    }
+
+
+    /**
+     * 日付変換処理
+     *
+     * ymdを日付フォーマット(MM/DD)に変換する
+     *
+     * @return 日付フォーマットに変換されたymd
+     */
+    fun ymdFormatDate(): String {
+        return ymd!!.substring(4, 6) + "/" + ymd!!.substring(6, 8)
+    }
+
+
+    /**
+     * カテゴリーアイコン変換処理
+     *
+     * categoryと一致するアイコンの画像パスを取得
+     *
+     * @return categoryと一致するアイコンの画像パス
+     */
+    fun categoryFormatPngSrc(): String {
+        return Const.NEWS_CATEGORY_ICON_SRC_ARRAY[category!!.toInt() - 1]
+    }
+
+
+    /**
+     * HTML対応文字列変換処理
+     *
+     * contentをHTMLの改行とスペース対応させた文字列に変換する
+     *
+     * @return HTML対応したcontent
+     */
+    fun contentFormatBreakLine(): String? {
+        return CommonLogic().changeAfterBreakLine(content)
     }
 }
