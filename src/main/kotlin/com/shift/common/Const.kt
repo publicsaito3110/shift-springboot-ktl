6ebb8f7bd3e46857dc934ffeac7cac737aae529f
publicsaito3110 <public.business.saito3110@gmail.com>
2023-02-12 15:47:44 +0900

    fix: 定数の追加

--- src/main/kotlin/com/shift/common/Const.kt
+++ src/main/kotlin/com/shift/common/Const.kt
@@ -16,7 +16,9 @@ public class Const {
         //-----------
         const val NEWS_HOME_DISPLAY_NEW_ICON_LIMIT_DAY: Long = 14
         const val NEWS_HOME_DISPLAY_LIMIT: Int = 5
-        const val NEWS_HOME_SRC_NEW_ICON: String = "/"
+        const val NEWS_HOME_SRC_NEW_ICON: String = "/img/icon/new-icon.png"
+        val NEWS_CATEGORY_ICON_SRC_ARRAY: Array<String> = arrayOf("/img/icon/category-icon1.png", "/img/icon/category-icon2.png", "/img/icon/category-icon3.png")
+
 
         //-----------
         // Schedule
@@ -36,9 +38,21 @@ public class Const {
         //-------
         // その他
         //-------
+
+        // 役職
         const val ROLE_USER_ADMIN: String = "ROLE_ADMIN"
         const val ROLE_USER_GENERAL: String = "ROLE_GENERAL"
 
+        // 文字コード
+        const val CHARACTER_CODE_BREAK_LINE_ALL: String = "\r\n|\r|\n"
+        const val CHARACTER_SPACE: String = " "
+        const val CHARACTER_PERCENT: String = "%"
+
+        // HTMLタグ
+        const val HTML_SPACE: String = "&nbsp;";
+        const val HTML_BR: String = "<br>";
+
+        // セッション
         const val SESSION_KEYWORD_ACCOUNT_BEAN: String = "SESSION_KEY1_ACCOUNT_BEAN"
 
         //------------------
