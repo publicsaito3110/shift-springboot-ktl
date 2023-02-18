6c8257d35e35221f00110e85dbd9929ab821170b
publicsaito3110 <public.business.saito3110@gmail.com>
2023-02-18 18:12:14 +0900

    add: CalendarAllBeanの追加

---
+++ src/main/kotlin/com/shift/domain/model/bean/CalendarAllBean.kt
@@ -0,0 +1,29 @@
+package com.shift.domain.model.bean
+
+import com.shift.domain.model.entity.ScheduleTimeEntity
+import lombok.AllArgsConstructor
+import lombok.NoArgsConstructor
+
+
+/**
+ * @author saito
+ *
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+class CalendarAllBean {
+
+    var year = 0
+
+    var month = 0
+
+    var calendarList: List<Int>? = null
+
+    var scheduleUserAllArray: Array<Array<String>>? = null
+
+    var afterYm: String? = null
+
+    var beforeYm: String? = null
+
+    var scheduleTimeEntity: ScheduleTimeEntity? = null
+}
