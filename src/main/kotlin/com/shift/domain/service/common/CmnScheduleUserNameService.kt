1b9dd5ee1db7a4e18c4d642a574b8926a886f7b7
publicsaito3110 <public.business.saito3110@gmail.com>
2023-02-20 17:24:36 +0900

    fix: 確定スケジュール登録済み全ユーザ取得処理の修正

--- src/main/kotlin/com/shift/domain/service/common/CmnScheduleUserNameService.kt
+++ src/main/kotlin/com/shift/domain/service/common/CmnScheduleUserNameService.kt
@@ -3,12 +3,15 @@ package com.shift.domain.service.common
 import com.shift.common.CmnScheduleLogic
 import com.shift.common.CommonLogic
 import com.shift.common.CommonUtil
+import com.shift.common.Const
+import com.shift.domain.model.bean.CmnScheduleUserNameBean
 import com.shift.domain.model.dto.ScheduleUserNameDto
 import com.shift.domain.model.entity.ScheduleTimeEntity
 import com.shift.domain.repository.ScheduleTimeRepository
 import com.shift.domain.repository.ScheduleUserNameRepository
 import com.shift.domain.service.BaseService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -30,7 +33,7 @@ class CmnScheduleUserNameService: BaseService() {
 
 
     /**
-     * [共通 Service] 確定スケジュール登録済み全ユーザ処理
+     * [共通 Service] 確定スケジュール登録済み全ユーザ取得処理
      *
      * @param year 年
      * @param month 月
@@ -41,125 +44,111 @@ class CmnScheduleUserNameService: BaseService() {
      */
     fun generateScheduleAllUser(year: Int, month: Int, lastDateYmd: String): CmnScheduleUserNameBean {
 
-        // ユーザ毎の確定スケジュールをユーザ名で取得
-        val scheduleUserNameDtoList: List<ScheduleUserNameDto> = selectScheduleAll(year, month)
+        // ユーザ毎の1ヶ月の確定スケジュールを取得
+        val scheduleUserNameDtoList: List<ScheduleUserNameDto> = selectScheduleUserNameMonth(year, month)
         // スケジュール時間区分を取得
-        val scheduleTimeEntity = selectScheduleTime(lastDateYmd)
+        val scheduleTimeEntity: ScheduleTimeEntity? = selectScheduleTime(lastDateYmd)
         // 確定スケジュールを判定した配列に変換
-        val userScheduleAllArray =
-            calcUserALLScheduleArrayBySchedule(scheduleUserNameDtoList, scheduleTimeEntity)
+        val scheduleUserNameArray: Array<Array<String?>> = calcScheduleUserNameArray(scheduleUserNameDtoList, scheduleTimeEntity)
 
-        //Beanにセット
-        return CmnScheduleUserNameBean(scheduleTimeEntity, userScheduleAllArray)
+        // Beanにセット
+        val cmnScheduleUserNameBean = CmnScheduleUserNameBean()
+        cmnScheduleUserNameBean.scheduleTimeEntity = scheduleTimeEntity
+        cmnScheduleUserNameBean.scheduleUserNameArray = scheduleUserNameArray
+        return cmnScheduleUserNameBean
     }
 
 
     /**
      * 確定スケジュール登録済みユーザArray処理
      *
-     * scheduleEntityListとscheduleTimeListから登録済みのスケジュールとスケジュール時間区分を取得し、登録されているユーザをArrayで取得する<br></br>
-     * ただし、scheduleTimeListの要素数だけ判別する(最大7まで)<br></br>
-     * 日付とスケジュール時間区分に登録しているユーザが存在するとき、ユーザー名を格納する(ユーザ毎に改行タグ&lt;br&gt;が追加される)
+     * 登録済みのスケジュールとスケジュール時間区分から該当の日付とスケジュール時間区分に登録されている全てのユーザを取得する<br>
+     * ただし、登録可能なスケジュール時間区分だけ判別する<br>
+     * 日付とスケジュール時間区分に登録しているユーザが存在するとき、ユーザー名が格納される (ユーザ毎に改行タグ&lt;br&gt;が追加される)
      *
-     *
-     * @param scheduleUserNameDtoList DBから取得したList<ScheduleUserNameDto> (List&lt;ScheduleUserNameDto&gt;)
-     * @param scheduleTimeList DBから取得したList<ScheduleTimeEntity> (List&lt;ScheduleTimeEntity&gt;)
-     * @return String[][]<br></br>
-     * エレメント(String[日付(31固定)][スケジュール時間(スケジュール登録可能数)])<br></br>
-     * 日付とスケジュール時間区分に登録しているユーザ名が順次格納される
-    </ScheduleTimeEntity></ScheduleUserNameDto> */
-    private fun calcUserALLScheduleArrayBySchedule(
-        scheduleUserNameDtoList: List<ScheduleUserNameDto>,
-        scheduleTimeEntity: ScheduleTimeEntity
-    ): Array<Array<String?>> {
+     * @param scheduleUserNameDtoList ユーザ毎の確定スケジュール
+     * @param scheduleTimeEntity スケジュール時間区分
+     * @return Array<Array<String?>> 日付とスケジュール時間区分に登録されている全てのユーザ<br>
+     * エレメント(要素 [日付(31固定)][スケジュール時間(スケジュール登録可能数)])
+     **/
+    private fun calcScheduleUserNameArray(scheduleUserNameDtoList: List<ScheduleUserNameDto>, scheduleTimeEntity: ScheduleTimeEntity?): Array<Array<String?>> {
 
-        //スケジュールに登録されているユーザを格納するための変数(要素[日付][スケジュール時間区分])
-        val userScheduleAllArray = Array(31) {
+        // スケジュールに登録されているユーザを格納するための変数 (要素[日付][スケジュール時間区分])
+        val scheduleUserNameArray = Array(31) {
             arrayOfNulls<String>(
                 Const.SCHEDULE_RECORDABLE_MAX_DIVISION
             )
         }
 
-        //スケジュールの判定を行うための共通クラス
+        // スケジュールの判定を行う共通ロジック
         val cmnScheduleLogic = CmnScheduleLogic()
 
-        //scheduleUserNameDtoの要素数(ユーザ数)だけループする
+        // スケジュールに登録されているユーザだけループ
         for (scheduleUserNameDto in scheduleUserNameDtoList) {
 
-            //登録されているユーザ名を取得
-            val userName: String = scheduleUserNameDto.getUserName()
+            // 登録されているユーザ名を取得
+            val userName: String? = scheduleUserNameDto.userName
 
-            //ユーザの1ヵ月分のスケジュールを日付ごとに取得
-            val scheduleDayList: List<String> = scheduleUserNameDto.getDayList()
+            // 1ヵ月分のスケジュールを日付ごとに取得
+            val scheduleDayList: List<String?> = scheduleUserNameDto.getScheduleDayList()
 
-            //scheduleDayList(日付の回数)だけループする
+            // スケジュールの日付だけループ
             for (i in scheduleDayList.indices) {
 
-                //日付ごとのスケジュールを取得し、スケジュール時間ごとにスケジュールが登録されているかどうかを判定したBooleanの配列を取得
+                // 日付ごとのスケジュールを取得し、スケジュール時間ごとにスケジュールが登録されているかどうかを判定したBooleanの配列を取得
                 val scheduleDay = scheduleDayList[i]
-                val isScheduleRecordedArray: Array<Boolean?> =
-                    cmnScheduleLogic.toIsScheduleRecordedArrayBySchedule(scheduleDay, scheduleTimeEntity)
+                val isScheduleRecordedArray: Array<Boolean?> = cmnScheduleLogic.toIsScheduleArray(scheduleDay, scheduleTimeEntity)
 
-                //isScheduleRecordedArray(スケジュール時間の区分)だけループする
+                // 判定した日付のスケジュール時間の区分だけループ
                 for (j in isScheduleRecordedArray.indices) {
 
-                    //isScheduleRecordedArray[j]がnull(スケジュール時間区分がない)とき、何もせずisScheduleRecordedArrayのループに戻る
+                    // 対象のスケジュール時間区分[j]がnullのとき、ループに戻る
                     if (isScheduleRecordedArray[j] == null) {
                         continue
                     }
 
-                    //スケジュールが登録されているとき
+                    // スケジュールが登録されているとき
                     if (isScheduleRecordedArray[j]!!) {
 
-                        //スケジュールに登録されているユーザに現在のユーザを改行タグ(<br>)と共に追加する
-                        val userSchedule =
-                            CommonUtil.changeEmptyByNull(userScheduleAllArray[i][j]) + userName + Const.HTML_TAG_BR
-                        userScheduleAllArray[i][j] = userSchedule
+                        // スケジュールに登録されているユーザに現在のユーザを改行タグと共に追加する
+                        scheduleUserNameArray[i][j] = CommonUtil.changeEmptyByNull(scheduleUserNameArray[i][j]) + userName + Const.HTML_BR
                     }
                 }
             }
         }
-        return userScheduleAllArray
+        return scheduleUserNameArray
     }
 
 
     /**
-     * [DB]確定スケジュール登録済みユーザ検索処理
+     *  [Repository] 月次確定スケジュール登録済みユーザ検索処理
      *
+     * 該当年月に一致するスケジュールに登録済みの全ユーザのスケジュールをユーザ毎に取得する<br>
+     * ただし、該当年月のスケジュールに登録済みのユーザがいないときはEmptyとなる
      *
-     * year, monthから1ヵ月分のスケジュールを取得する
-     *
-     * @param year LocalDateから取得した年(int)
-     * @param month LocalDateから取得した月(int)
-     * @return List<ScheduleUserNameDto> <br></br>
-     * フィールド(List&lt;ScheduleUserNameDto&gt;)<br></br>
-     * id, ymd, userName, day1, day2, day3... day30, day31
-    </ScheduleUserNameDto> */
-    private fun selectScheduleAll(year: Int, month: Int): List<ScheduleUserNameDto> {
+     * @param year 検索したい年
+     * @param month 検索したい月
+     * @return List<ScheduleUserNameDto> ユーザ毎の確定スケジュール
+     **/
+    private fun selectScheduleUserNameMonth(year: Int, month: Int): List<ScheduleUserNameDto> {
 
-        //year, monthをym(YYYYMM)に変換
-        val ym: String = CommonLogic().toStringYmFormatSixByYearMonth(year, month)
-
-        //DBから取得し、返す
-        return scheduleUserNameRepository.selectScheduleUserNameByYm(ym)
+        // ymに変換
+        val ym = CommonLogic().toStringYm(year, month)
+        return scheduleUserNameRepository.selectScheduleUserNameMonth(ym)
     }
 
 
     /**
-     * [DB]スケジュール時間区分取得処理
+     * [Repository] スケジュール時間区分検索処理
      *
-     *
-     * 取得したい日付(ymd)から該当するスケジュール時間区分を取得する<br></br>
-     * また、現在日(ymd)に該当するスケジュール時間区分が複数登録されているときは最新のスケジュール時間区分が取得される<br></br>
+     * 取得したい日付(ymd)から該当するスケジュール時間区分を取得する<br>
+     * また、現在日(ymd)に該当するスケジュール時間区分が複数登録されているときは最新のスケジュール時間区分が取得される<br>
      * ただし、スケジュール時間区分が何も登録されていないときはnullとなる
      *
-     *
      * @param ymd 取得したいスケジュール時間区分の日付(YYYYMMDD)
-     * @return ScheduleTimeEntity<br></br>
-     * フィールド(ScheduleTimeEntity)<br></br>
-     * id, endYmd, name1, startHm1, endHM1, restHm1... startHm7, endHM7, restHm7
-     */
-    private fun selectScheduleTime(ymd: String): ScheduleTimeEntity {
-        return scheduleTimeRepository.selectScheduleTimeByYmd(ymd)
+     * @return ScheduleTimeEntity スケジュール時間区分
+     **/
+    private fun selectScheduleTime(ymd: String): ScheduleTimeEntity? {
+        return scheduleTimeRepository.selectScheduleTime(ymd)
     }
 }
