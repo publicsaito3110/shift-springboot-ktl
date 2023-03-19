9ebdc4c9099305712f2902142370f19b7f00beca
publicsaito3110 <public.business.saito3110@gmail.com>
2023-03-19 19:52:43 +0900

    fix: Model:UIの修正

--- src/main/kotlin/com/shift/controller/ScheduleDecisionController.kt
+++ src/main/kotlin/com/shift/controller/ScheduleDecisionController.kt
@@ -44,4 +44,25 @@ class ScheduleDecisionController : BaseController() {
         // View
         return "view/schedule-decision/schedule-decision"
     }
+    
+    
+    /**
+     * [Controller] 確定スケジュール修正機能画面 (/schedule-decision/modify)
+     *
+     * @param ym RequestParameter 年月
+     * @param authentication Authentication ユーザ情報
+     * @param model Model ThymeleafのUI
+     * @return String Viewのパス
+     */
+    @RequestMapping("/schedule-decision/modify")
+    fun scheduleDecisionModify(@RequestParam(value = "ym", required = false) ym: String?, authentication: Authentication?, model: Model): String? {
+
+        // Service
+        val scheduleDecisionModifyBean: ScheduleDecisionModifyBean = scheduleDecisionService.scheduleDecisionModify(ym)
+        model.addAttribute("bean", scheduleDecisionModifyBean)
+        model.addAttribute("htmlColorArray", Const.SCHEDULE_HTML_CLASS_DISPLAY_COLOR_ARRAY)
+        model.addAttribute("htmlBgColorArray", Const.SCHEDULE_HTML_CLASS_DISPLAY_BG_COLOR_ARRAY)
+        // View
+        return "view/schedule-decision/schedule-decision-modify"
+    }
 }
