674dded1e3e82e690c00e4a06611a8c46eb55531
publicsaito3110 <public.business.saito3110@gmail.com>
2023-03-24 19:29:03 +0900

    add: SQL:未退職ユーザ検索処理の追加

--- src/main/kotlin/com/shift/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/shift/domain/repository/UserRepository.kt
@@ -22,6 +22,19 @@ interface UserRepository: BaseRepository<UserEntity, String> {
      * @param userId 取得したいユーザのユーザID
      * @return UserEntity ユーザ情報
      */
-    @Query(value = "SELECT u.* FROM user u WHERE u.id = :userId;", nativeQuery = true)
+    @Query(value = "SELECT u.* FROM user u WHERE u.id = :userId", nativeQuery = true)
     fun selectUser(userId: String?): UserEntity?
+
+
+    /**
+     * [Repository] ユーザ検索処理
+     *
+     * 未退職ユーザを全て取得する<br>
+     * ただし、該当するユーザーがいない場合はEmptyとなる
+     *
+     * @param delFlg 退職フラグ
+     * @return UsersEntity 全ての未退職ユーザ
+     */
+    @Query(value = "SELECT u.* FROM users u WHERE u.del_flg != :delFlg OR u.del_flg IS NULL", nativeQuery = true)
+    fun selectUsersNotDelFlg(delFlg: String?): List<UserEntity>
 }
